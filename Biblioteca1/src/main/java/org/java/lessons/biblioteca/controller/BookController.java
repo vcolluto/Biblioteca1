ad5e642f45cdc125428279c6313754deea26bad6
vcolluto <86895240+vcolluto@users.noreply.github.com>
2023-02-15 12:06:02 +0100

    aggiunta pagina di dettaglio

--- Biblioteca1/src/main/java/org/java/lessons/biblioteca/controller/BookController.java
+++ Biblioteca1/src/main/java/org/java/lessons/biblioteca/controller/BookController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -19,11 +20,17 @@ public class BookController {
 	
 	
 	@GetMapping()
-	public String index(Model model) {
-		
+	public String index(Model model) {		
 		List<Book> res=repository.findAll();
 		model.addAttribute("elencoLibri", res);
 		return "books/index";
+	}
+	
+	@GetMapping("/{id}")		//gestirà le richieste GET di tipo /books/x  (x è l'id del libro)
+	public String detail(@PathVariable("id") Integer id, Model model) {
+		Book book=repository.getReferenceById(id);
+		model.addAttribute("book", book);
+		return "books/detail";
 	}
 	
 }
