9d6ffcf413ead30db8f3559baaac53f6d676865a
vcolluto <86895240+vcolluto@users.noreply.github.com>
2023-02-15 10:56:40 +0100

    aggiunto libro

--- Biblioteca1/src/main/resources/import.sql
+++ Biblioteca1/src/main/resources/import.sql
@@ -1,3 +1,4 @@
 INSERT INTO libreria.books(author, isbn_code, title) VALUES('Tolkien', '1234546565', 'Il signore degli anelli');
 INSERT INTO libreria.books(author, isbn_code, title) VALUES('Ammaniti', '4343243233', 'Io non ho paura');
 INSERT INTO libreria.books(author, isbn_code, title) VALUES('Matheson', '7657657676', 'Io sono leggenda');
+INSERT INTO libreria.books(author, isbn_code, title) VALUES('Rowling', '5423432433', 'Harry Potter e la pietra filosofale');
