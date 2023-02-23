e0c03f4456bfffed06f22cb3099eb4d45cedbf69
vcolluto <86895240+vcolluto@users.noreply.github.com>
2023-02-23 13:01:44 +0100

    aggiunte viste elenco categorie e nuova categoria

---
+++ Biblioteca1/src/main/java/org/java/lessons/biblioteca/controller/CategoryController.java
@@ -0,0 +1,59 @@
+package org.java.lessons.biblioteca.controller;
+
+import java.util.List;
+
+import org.java.lessons.biblioteca.model.Book;
+import org.java.lessons.biblioteca.model.Category;
+import org.java.lessons.biblioteca.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.validation.Valid;
+
+@Controller
+@RequestMapping("/categories")
+public class CategoryController {
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
+	@GetMapping()		// GET /categories
+	public String index(			
+			Model model) {	
+		List<Category> res = categoryRepository.findAll(Sort.by("name"));	//tutti le categorie ordinate per nome
+		model.addAttribute("elencoCategorie", res);
+		return "categories/index";
+	}
+
+	@GetMapping("/create")		//gestirà le richieste GET di tipo /categories/create
+	public String create(Model model) {
+		Category category=new Category();	//non esiste ancora sul DB
+		
+		model.addAttribute("category", category);
+	
+		return "categories/edit";
+	}
+	
+	@PostMapping("/create")  	//gestirà le richieste di tipo POST di tipo /books/create
+	public String store(
+		@Valid @ModelAttribute("category") Category formCategory, 
+		BindingResult bindingResult,
+		Model model){
+	
+		if (bindingResult.hasErrors())
+			return "categories/edit";
+		
+		categoryRepository.save(formCategory);
+		
+		return "redirect:/categories"; //genera un altro get
+		
+	}
+}
